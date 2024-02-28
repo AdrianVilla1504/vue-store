65d802c98edea21cea5cba42f1592becccbc8655
AdrianVilla1504 <adriancvilla@gmail.com>
2024-02-27 23:45:50 -0500

    feature: configure store, router, and header importation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Header :cartItemCount="cartItemCount" />
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer>
+      <p>&copy; {{ new Date().getFullYear() }} Mi Tienda en Línea</p>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue'
+import router from './router'
+import Header from './components/Header.vue'
+import { useCartStore } from './store'
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Header
+  },
+  setup() {
+    const cartStore = useCartStore()
+
+    return {
+      router,
+      cartItemCount: cartStore.itemCount
+    }
   }
-}
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  main {
+    flex-grow: 1; 
+    padding: 20px;
+  }
+  footer {
+    background-color: #333;
+    color: #fff;
+    padding: 10px 20px;
+    text-align: center;
+    margin-top: auto; 
+  }
 </style>
+
