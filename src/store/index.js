92b4b657097e03b3c7e0b4b8ebad80f7c7e4e61b
AdrianVilla1504 <adriancvilla@gmail.com>
2024-02-27 23:48:30 -0500

    feature: add services for fetching data and store for state management

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+
+export const useCartStore = defineStore({
+  id: 'cart',
+  state: () => ({
+    items: [],
+  }),
+  getters: {
+    itemCount(state) {
+      return state.items.reduce((total, item) => total + item.quantity, 0);
+    },
+  },
+  actions: {
+    addItem(product) {
+      const existingItem = this.items.find(item => item.id === product.id);
+      console.log("this.items => ", this.items)
+      if (existingItem) {
+        alert('El producto ya ha sido agregado al carrito.');
+      } else {
+        this.$state.items.push({ ...product, quantity: 1 });
+      }
+    },
+    removeItem(itemId) {
+      this.$state.items = this.$state.items.filter(item => item.id !== itemId);
+    },
+    updateItemQuantity({ id, quantity }) {
+      const itemIndex = this.$state.items.findIndex(item => item.id === id);
+      if (itemIndex !== -1) {
+        this.$state.items[itemIndex].quantity = quantity;
+      }
+    },
+  },
+});
